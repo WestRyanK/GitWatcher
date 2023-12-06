216b80079f6e0920dab82f8ef47ac6eae3c1eb58
Ryan West <west.ryan.k@gmail.com>
2023-12-06 12:09:51 -0700

    Register FileSystemWatcher on current directory

---
+++ GitGraphWatcher.ps1
@@ -0,0 +1,22 @@
+Function Register-FileSystemWatcher {
+    param (
+        [string] $Folder,
+        [scriptblock] $Action
+    )
+
+    $Folder = $Folder | Resolve-Path
+    $Filter = "*.*"
+    $Watcher = New-Object System.IO.FileSystemWatcher $Folder, $Filter -Property @{
+        IncludeSubdirectories = $True
+        EnableRaisingEvents = $True
+    }
+
+    Register-ObjectEvent $Watcher -EventName "Changed" -Action $Action
+    Register-ObjectEvent $Watcher -EventName "Created" -Action $Action
+    Register-ObjectEvent $Watcher -EventName "Deleted" -Action $Action
+}
+
+Register-FileSystemWatcher (Get-Location) -Action {
+    Write-Host $Event.SourceEventArgs.ChangeType
+    Write-Host $Event.SourceEventArgs.FullPath
+}
