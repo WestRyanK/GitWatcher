a0459db545032c4afa19d9774c8280a033cda389
Ryan West <west.ryan.k@gmail.com>
2024-08-06 10:17:44 -0600

    Use special folder path for install
    The My Documents folder isn't always located in ~/Documents. So
    use GetFolderPath to get the correct location.

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,8 @@
 $ScriptName = "GitWatcher"
-$DestinationPath = "$Home/Documents/PowerShell/Modules/$ScriptName"
+$Documents = [Environment]::GetFolderPath("MyDocuments")
+$DestinationPath = [IO.Path]::Combine("$Documents", "PowerShell", "Modules", "$ScriptName")
 $null = New-Item -Path $DestinationPath -ItemType Directory -Force
-Copy-Item -Path "$PSScriptRoot/$ScriptName.psm1" -Destination "$DestinationPath" -Recurse -Force
+$SourcePath = [IO.Path]::Combine("$PSScriptRoot", "$ScriptName.psm1")
+Copy-Item -Path $SourcePath -Destination "$DestinationPath" -Recurse -Force
 Import-Module GitWatcher -Force
 Write-Host "Installed GitWatcher module in '$DestinationPath'"
