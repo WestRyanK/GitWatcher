403319bf9138f8159e9d48978e604df7b924f418
Ryan West <west.ryan.k@gmail.com>
2025-04-09 14:52:33 -0600

    Add initial window positioning proof of concept
    The overall plan with the new .NET GitWatcher is to create a borderless
    window that automatically positions itself to the right of the Terminal
    which is running in Quake Mode. The width of the Terminal will be
    adjusted by GitWatcher so that the Quake Terminal will take up the left
    half of the screen and the GitWatcher window will take up the right half
    of the screen.
    
    This commit roughly implements this idea as a proof of concept. Further
    refactoring should be done to make this implementation sustainable.

--- GitWatcher/GitWatcher/MainWindow.xaml.cs
+++ GitWatcher/GitWatcher/MainWindow.xaml.cs
@@ -1,4 +1,15 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace GitWatcher
 {
@@ -10,6 +21,78 @@ namespace GitWatcher
         public MainWindow()
         {
             InitializeComponent();
+
+
+            UpdateLoop();
         }
+
+        private async Task UpdateLoop()
+        {
+            while (true)
+            {
+                WindowStatus status = GetWindowState("CASCADIA_HOSTING_WINDOW_CLASS", null);
+                UpdateTerminalWindowState(status);
+                UpdateGitWatcherWindowState(status);
+                await Task.Delay(250);
+            }
+        }
+
+        private void UpdateTerminalWindowState(WindowStatus status)
+        {
+            int height = (int)(status.WindowMax.Y - status.WindowMin.Y);
+            WindowsAPI.MoveWindow(status.Handle, (int)status.WindowMin.X, (int)status.WindowMin.Y, 600, height, true);
+        }
+
+        private void UpdateGitWatcherWindowState(WindowStatus status)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                if (status.IsMinimized)
+                {
+                    this.WindowState = WindowState.Minimized;
+                    return;
+                }
+                else
+                {
+
+
+
+                    this.WindowState = WindowState.Normal;
+                    this.Width = (status.WindowMax.X - status.WindowMin.X) * .5f;
+                    this.Height = status.WindowMax.Y - status.WindowMin.Y;
+                    this.Left = status.WindowMin.X + this.Width;
+                    this.Top = status.WindowMin.Y;
+                }
+            });
+        }
+
+        private WindowStatus GetWindowState(string? windowClass, string? windowTitle)
+        {
+            nint windowHandle = WindowsAPI.FindWindow(windowClass, windowTitle);
+            WindowsAPI.GetWindowRect(windowHandle, out WindowsAPI.RECT rect);
+            WindowsAPI.WINDOWPLACEMENT placement = new();
+            WindowsAPI.GetWindowPlacement(windowHandle, ref placement);
+
+            Debug.WriteLine($"Window: ({rect.Left}, {rect.Top}) ({rect.Right}, {rect.Bottom})");
+            Debug.WriteLine($"Placement: {placement.ptMinPosition} {placement.ptMaxPosition} {placement.rcNormalPosition} {placement.showCmd} {placement.flags} {placement.length}");
+
+            return new WindowStatus(windowHandle, new Point(rect.Left, rect.Top), new Point(rect.Right, rect.Bottom), placement.showCmd == WindowsAPI.ShowWindowCommands.Minimized);
+        }
+    }
+
+    struct WindowStatus
+    {
+        public WindowStatus(nint handle, Point windowMin, Point windowMax, bool isMinimized)
+        {
+            Handle = handle;
+            WindowMin = windowMin;
+            WindowMax = windowMax;
+            IsMinimized = isMinimized;
+        }
+
+        public IntPtr Handle { get; }
+        public Point WindowMin { get; }
+        public Point WindowMax { get; }
+        public bool IsMinimized { get; }
     }
 }
---
+++ GitWatcher/GitWatcher/WindowsAPI.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GitWatcher;
+internal class WindowsAPI
+{
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string? lpClassName, string? lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;    // x position of upper-left corner
+        public int Top;     // y position of upper-left corner
+        public int Right;   // x position of lower-right corner
+        public int Bottom;  // y position of lower-right corner
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT
+    {
+        public int length;
+        public int flags;
+        public ShowWindowCommands showCmd;
+        public System.Drawing.Point ptMinPosition;
+        public System.Drawing.Point ptMaxPosition;
+        public System.Drawing.Rectangle rcNormalPosition;
+    }
+
+    public enum ShowWindowCommands
+    {
+        Hide = 0,
+        Normal = 1,
+        Minimized = 2,
+        Maximized = 3
+    }
+}
